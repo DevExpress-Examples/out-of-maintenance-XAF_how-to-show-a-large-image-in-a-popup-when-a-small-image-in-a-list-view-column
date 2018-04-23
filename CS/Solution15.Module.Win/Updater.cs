c2b4cb000dfbbf2c884510a8b5d91a40c2cbe2e5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Solution15.Module.Win/Updater.cs
@@ -0,0 +1,14 @@
+using System;
+
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+
+namespace Solution15.Module.Win {
+    public class Updater : ModuleUpdater {
+        public Updater(ObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
+        public override void UpdateDatabaseAfterUpdateSchema() {
+            base.UpdateDatabaseAfterUpdateSchema();
+        }
+    }
+}
---
+++ VB/Solution15.Win/Program.vb
@@ -0,0 +1,45 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Configuration
+Imports System.Windows.Forms
+
+Imports DevExpress.ExpressApp
+Imports DevExpress.ExpressApp.Security
+Imports DevExpress.ExpressApp.Win
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace Solution15.Win
+	Friend NotInheritable Class Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		Private Sub New()
+		End Sub
+		<STAThread> _
+		Shared Sub Main()
+#If EASYTEST Then
+			DevExpress.ExpressApp.EasyTest.WinAdapter.RemotingRegistration.Register(4100)
+#End If
+
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			EditModelPermission.AlwaysGranted = System.Diagnostics.Debugger.IsAttached
+			Dim winApplication As New Solution15WindowsFormsApplication()
+#If EASYTEST Then
+			If ConfigurationManager.ConnectionStrings("EasyTestConnectionString") IsNot Nothing Then
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("EasyTestConnectionString").ConnectionString
+			End If
+#End If
+			If ConfigurationManager.ConnectionStrings("ConnectionString") IsNot Nothing Then
+				winApplication.ConnectionString = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+			End If
+			Try
+				winApplication.Setup()
+				winApplication.Start()
+			Catch e As Exception
+				winApplication.HandleException(e)
+			End Try
+		End Sub
+	End Class
+End Namespace
